4a2e6532f2c6855aa689c0bd223e4cee528fc476
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-22 18:17:04 -0800

    added the function of the button to the js file

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Add a button element to the HTML file.
+
 // Add an event listener to the button that displays an alert when the button is clicked.
+
 // Can you make it so that this event listener only triggers the first time that the button is clicked and then never again?
+
 // Add a link element to the HTML file that goes to your favorite website.
+
 // Add an event listener to the link that prevents it from navigating to the website when you click it.
+
 // Add more code to the event listener for the link that pops up an alert that says “No distractions! I’m coding!”
+
+function buttonClick () {
+    console.log(" ");
+}
+
+let button = document.querySelector("button");
+
+button.addEventListener("click", buttonClick);
